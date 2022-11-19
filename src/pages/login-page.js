802e64a75adb78d265e4160374526fc8887f4a71
Catode <zerotyr3@gmail.com>
2022-11-19 11:22:31 -0500

    Add: login view & funtion

--- src/pages/login-page.js
+++ src/pages/login-page.js
@@ -1,8 +1,69 @@
+import { input } from "../components/input.js";
+import DOMHandler from "../../dom-handler.js";
+import HomePage from "./home-page.js";
+import { login } from "../services/sessions-service.js";
+
+function render() {
+    return `
+    <main class="section">
+        <section class="conteiner">
+            <h1 class="heading heading--lg text-center mb-4">login</h1>
+            <form class="flex flex-column gap-4 mb-4 js-login-form">
+                ${input({
+                    id: "email",
+                    label: "email",
+                    placeholder: "john@example.com",
+                    type: "email",
+                    required: true,
+                    name: "email",
+                    value: "loquequieras@gmail.com",
+                })}
+
+                ${input({
+                    label: "password",
+                    id: "password",
+                    name: "password",
+                    placeholder: "********",
+                    type: "password",
+                    required: true,
+                    value: "123456",
+                })}
+            
+                <button type="submit" class="button button--primary">Login</button>
+            </form>
+            <a href="#" class="block text-center js-singup-link">Create account</a>
+        </section>
+    </main>
+  `;
+}
+
+
+function listenSubmitForm() {
+    const form = document.querySelector(".js-login-form");
+
+    form.addEventListener("submit", async (event) => {
+      event.preventDefault();
+  
+      const { email, password } = event.target;
+  
+      const credentials = {
+        email: email.value,
+        password: password.value
+      };
+
+      const user = await login(credentials);
+      DOMHandler.load(HomePage);
+    });
+  }
+  
+
 const LoginPage = {
     toString() {
-        return `<h1>Login Page</h1>`
+        return render();
     },
-    addListeners() {}
+    addListeners() {
+        listenSubmitForm();
+    }
 }
 
 export default LoginPage
