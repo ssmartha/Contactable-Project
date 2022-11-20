a563cb3a4765f498b849d87e00dcaf36e3899955
Martha Huaman <ms.huamann@gmail.com>
2022-11-19 19:21:10 -0500

    ADD: edit contact page

--- src/pages/login-page.js
+++ src/pages/login-page.js
@@ -2,6 +2,8 @@ import { input } from "../components/input.js";
 import DOMHandler from "../../dom-handler.js";
 import HomePage from "./home-page.js";
 import { login } from "../services/sessions-service.js";
+// import EditContact from "./edit_contact.js"
+import EditContactPage from "./edit-contact-page.js";
 
 function render() {
     return `
@@ -28,7 +30,7 @@ function render() {
                     required: true,
                     value: "123456",
                 })}
-            
+
                 <button type="submit" class="button button--primary">Login</button>
             </form>
             <a href="#" class="block text-center js-singup-link">Create account</a>
@@ -43,19 +45,20 @@ function listenSubmitForm() {
 
     form.addEventListener("submit", async (event) => {
       event.preventDefault();
-  
+
       const { email, password } = event.target;
-  
+
       const credentials = {
         email: email.value,
         password: password.value
       };
 
       const user = await login(credentials);
-      DOMHandler.load(HomePage);
+        // DOMHandler.load(HomePage);
+      DOMHandler.load(EditContactPage);
     });
   }
-  
+
 
 const LoginPage = {
     toString() {
